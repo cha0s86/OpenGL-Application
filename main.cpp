f3f939233e5a01dd1c9d9390a525a0771b1d83dc
cha0s86 <pekkakukonlehto@outlook.com>
2025-04-26 12:22:43 +0300

    My OpenGL window!

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@
 
 int main() {
 
-	
+	// Your OpenGL code here
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+
+	Sleep(5000);
 
 	return 0;
 }

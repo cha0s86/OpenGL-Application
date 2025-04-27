e17d0d3575ebc64566dea812ff6c86db6763d851
cha0s86 <pekkakukonlehto@outlook.com>
2025-04-27 09:20:29 +0300

    Git commit

--- main.cpp
+++ main.cpp
@@ -6,14 +6,41 @@
 int main() {
 
 	// Your OpenGL code here
-	if (!glfwInit()) {
-		std::cerr << "Failed to initialize GLFW" << std::endl;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
 		return -1;
 	}
 
-	glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+	glfwMakeContextCurrent(window);
 
-	Sleep(5000);
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		if (GetAsyncKeyState(VK_ESCAPE)) {
+			glfwSetWindowShouldClose(window, true);
+		}
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
